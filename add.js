220e95212e0e13c9f237d9ef0dda547606655457
Sai Kumar V R <joesphkurvila100@gmail.com>
2022-09-28 14:20:46 +0530

    logic created

---
+++ add.js
@@ -0,0 +1,8 @@
+function add()
+ {
+var num1=document.getElementaryById("firstno").value
+var num2=document.getElementaryById("second").value
+
+var result=num1+num2
+doucment.getElementaryById("result").value=result
+}
