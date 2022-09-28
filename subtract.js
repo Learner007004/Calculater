953721c7f683eafff1718bf1a0a8f57f7b7d06a1
Sai Kumar V R <joesphkurvila100@gmail.com>
2022-09-28 14:48:54 +0530

    changed

---
+++ subtract.js
@@ -0,0 +1,8 @@
+function sub()
+ {
+var num1=document.getElementaryById("firstno").value
+var num2=document.getElementaryById("second").value
+var num3=document.getElementaeyById("Thirdno").value
+var result =parseInt( num1)-parseInt(num2)-parseInt(num3)
+doucment.getElementaryById("result").value=result
+}
