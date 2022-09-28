f426c13cd1657f1fae07284022fa8dbfebe2f056
Sai Kumar V R <joesphkurvila100@gmail.com>
2022-09-28 14:30:37 +0530

    changed

--- add.js
+++ add.js
@@ -2,7 +2,7 @@ function add()
  {
 var num1=document.getElementaryById("firstno").value
 var num2=document.getElementaryById("second").value
-
-var result=num1+num2
+var num3=document.getElementaeyById("Thirdno").value
+var result =parseInt( num1)+parseInt(num2)+parseInt(num3)
 doucment.getElementaryById("result").value=result
 }
